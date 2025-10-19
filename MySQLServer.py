28d8c08423408a629f9962c2c23147c417c70c4f
irihojaphet <irihojaja@yahoo.com>
2025-10-19 23:33:15 +0200

    mysql server

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 
 def create_database():
@@ -23,7 +22,7 @@ def create_database():
             
             cursor.close()
             
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
         
     finally:
